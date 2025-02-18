ea6a87ced6195270b5306a60d987170a773181c6
Emmanuel Buckshi <adventurist@gmail.com>
2025-02-17 22:04:02 -0500

    better

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "include/kproto/ipc.hpp"
+#include <iostream>
 
 int main(int argc, char** argv)
 {
 
   auto msg = kiq::platform_request{"kiq", "1234", "logicp", "hello", "{\"key\": \"value\"}"};
+
+  std::cout << msg.to_string() << std::endl;
   return 0;
 }
 
